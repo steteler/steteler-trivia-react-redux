95b6a2ba733801e4c7fc0e768f7cb2f84fe42655
kikutii <joaovictorkikuti@gmail.com>
2022-04-18 22:02:05 -0300

    trocado nome de classe componente footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import '../style/Footer.css';
 class Footer extends Component {
   render() {
     return (
-      <footer className="trivia-footer">
+      <footer className="footer-container">
         <span>Feito com muito </span>
         <span role="img" aria-label="coffee">☕</span>
         <span> por: kikuti, tiago, kaio, samuca e leonardo.</span>
