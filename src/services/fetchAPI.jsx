3685109d95300fc6d1a203811f4b2d773d3c91be
kikutii <joaovictorkikuti@gmail.com>
2022-04-18 21:58:26 -0300

    refatorado requisição fetchAPI

--- src/services/fetchAPI.jsx
+++ src/services/fetchAPI.jsx
@@ -1,7 +1,8 @@
 export default (
   async function fetchAPI() {
-    const endPoint = 'https://opentdb.com/api_token.php?command=request';
-    const request = await fetch(endPoint);
+    const request = await fetch(
+      'https://opentdb.com/api_token.php?command=request',
+    );
     const { token } = await request.json();
     return token;
   }
