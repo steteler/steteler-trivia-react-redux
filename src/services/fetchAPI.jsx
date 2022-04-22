cd294fa7fdaa42ffa8cd5517f548b1c9af2614c0
kikutii <joaovictorkikuti@gmail.com>
2022-04-21 21:40:16 -0300

    trocado nome da função fetchAPI para getToken

--- src/services/fetchAPI.jsx
+++
@@ -1,9 +0,0 @@
-export default (
-  async function fetchAPI() {
-    const request = await fetch(
-      'https://opentdb.com/api_token.php?command=request',
-    );
-    const { token } = await request.json();
-    return token;
-  }
-);
