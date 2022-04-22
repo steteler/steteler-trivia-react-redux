46fbede8215d72510d9127cc12b2182ee9e9af35
kikutii <joaovictorkikuti@gmail.com>
2022-04-21 21:42:28 -0300

    refatorado page Feedback

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,109 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 
 class Feedback extends Component {
-  resectGame = () => {
-    const { history } = this.props;
-    history.push('/');
-  }
-
-  playRankingGame = () => {
-    const { history } = this.props;
-    history.push('/ranking');
-  }
-
-  //  feedbackQuote = () => {
-  //    const { rightOnes } = this.props;
-  //    const questValidation = 3;
-  //    switch (true) {
-  //    case rightOnes >= questValidation:
-  //      return 'Well Done!';
-  //    default:
-  //      return 'Could be better...';
-  //    }
-  //  }
-
-  //  homeRedirect = (event) => {
-  //    event.preventDefault();
-  //    const { history } = this.props;
-  //    history.push('/');
-  //  }
-
-  //  rankingRedirect = (event) => {
-  //    event.preventDefault();
-  //    const { history } = this.props;
-  //    history.push('/Ranking');
-  //  }
-
   render() {
-    // const { score } = this.props;
+    const { score, assertions } = this.props;
+    const minAssertions = 3;
     return (
-      <div>
+      <>
         <Header />
         <h1 data-testid="feedback-text">Text FeedBack</h1>
 
-        <button
-          onClick={ this.resectGame }
-          type="button"
-          data-testid="btn-play-again"
-        >
-          Play Again
-        </button>
-        <button
-          onClick={ this.playRankingGame }
-          type="button"
-          data-testid="btn-ranking"
-        >
-          Ranking
-        </button>
+        <h1 data-testid="feedback-text">
+          { assertions < minAssertions ? 'Could be better...' : 'Well Done!' }
+        </h1>
 
-        {/* <p> { score }</p> */}
-        {/* <section>
-          <h2 data-testid="feedback-text">{ this.feedbackQuote() }</h2>
-          <h3
-            data-testid="feedback-total-question"
-          >
-            {`Você acertou ${rightOnes} questões!`}
-          </h3>
-          <h3
-            data-testid="feedback-total-score"
-          >
-            {`Um total de ${score} pontos.`}
-          </h3>
+        <p data-testid="feedback-total-score">{ score }</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
+
+        <Link to="/">
           <button
             type="button"
             data-testid="btn-play-again"
-            onClick={ this.homeRedirect }
           >
             Play Again
           </button>
+        </Link>
+        <Link to="/ranking">
           <button
             type="button"
             data-testid="btn-ranking"
-            onClick={ this.rankingRedirect }
           >
             Ranking
           </button>
-
-        </section> */}
-      </div>
+        </Link>
+      </>
     );
   }
 }
 
-// Feedback.propTypes = {
-//   rightOnes: PropTypes.number,
-//   score: PropTypes.number,
-// }.isRequired;
-
 Feedback.propTypes = {
-  history: PropTypes.objectOf(PropTypes.any).isRequired,
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
-function mapStateToProps(state) {
-  return { score: state.player.score };
+function mapStateToProps({ player }) {
+  return {
+    score: player.score,
+    assertions: player.assertions,
+  };
 }
 
 export default connect(mapStateToProps)(Feedback);
